8b9e4e4d9522f6f48fc767dbd368e46acad53a3b
Methasit Tantiplubtong <nokia.methasit@gmail.com>
2022-11-06 13:55:13 -0800

    text callback

--- webapp/client/src/App.js
+++ webapp/client/src/App.js
@@ -2,27 +2,38 @@ import './App.css';
 import Input from './components/Input.js';
 import Navbar from './components/Navbar.js';
 import Profile from './components/Profile.js';
+import { useState } from 'react';
+
+
+
 
 function App() {
+  const [text, setText] = useState("");
+
+  function textHandler(string){
+    setText(string);
+  }
+
   return (
     <div className="App">
       <Navbar/>
+      <h1>{text}</h1>
       <div className='big_container'>
         <div className='input_outercontain'>
-        <Input/>
+        <Input textHandler = {textHandler}/>
         </div>
       <div className='profile_container'>
       <Profile
-        heading= "Gender"
-        description= "There is X% gender bias detected in the text. Changes are recommended."
+      heading= "Gender"
+      description= "There is X% gender bias detected in the text. Changes are recommended."
       />
       <Profile
-        heading= "Race"
-        description= "There is X% racial bias detected in the text. Changes are recommended."
+      heading= "Race"
+      description= "There is X% racial bias detected in the text. Changes are recommended."
       />
       <Profile
-        heading= "Disability"
-        description= "There is X% disability bias detected in the text. Changes are recommended."
+      heading= "Disability"
+      description= "There is X% disability bias detected in the text. Changes are recommended."
       />
       </div>
       </div>
