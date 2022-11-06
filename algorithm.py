39a6a2d5b57061c25cafabc82b5763c3ae1864c9
Methasit Tantiplubtong <nokia.methasit@gmail.com>
2022-11-06 09:47:08 -0800

    prototype for percentages

--- algorithm.py
+++ algorithm.py
@@ -4,13 +4,23 @@ import string
 text = input("Put in your text \n")
 text_array = text.split()
 
+total = 0
+feminine = 0
+masculine = 0
+race = 0
+
 
 for word in text_array:
     nopunc = word.translate(word.maketrans("","", string.punctuation))
     processed = nopunc.lower()
     if processed in dict.feminine:
-        print(processed + " " + str(dict.feminine[processed]))
+        feminine = feminine + 1
     if processed in dict.masculine:
-        print(processed + " " + str(dict.masculine[processed]))
-    else:
-        print(processed + " " + "neutral")
+        masculine = masculine + 1
+    if processed in dict.race:
+        race = race + 1
+    total = total + 1
+
+print("Percentage fem: " + str((feminine * 100)/total))
+print("Percentage mas: " + str((masculine * 100)/total))
+print("Percentage rac: " + str((race * 100)/total))
