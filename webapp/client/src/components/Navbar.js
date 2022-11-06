a3638a4b732c87f7cc29a216d5ac275a3e25edd0
Methasit Tantiplubtong <nokia.methasit@gmail.com>
2022-11-06 10:34:18 -0800

    navbar with name

---
+++ webapp/client/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Navbar.css';
+
+function Navbar(){
+    return(
+        <div className="navbar_container">
+            <div className="items_container">
+                <div>NAME</div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
