e52863142ba137e58cc8350842d936d49af5faac
Brian Wong <briann.wong@hotmail.com>
2022-11-06 12:06:13 -0800

    Merge branch 'main' of https://github.com/rachel-601/HackCamp-2022 into main

--- webapp/client/src/App.js
+++ webapp/client/src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
 import Input from './components/Input.js';
 import Navbar from './components/Navbar.js';
+import Profile from './componenets/Profile.js';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Input/>
+
+      <Profile
+      heading= "Gender"
+      description= "There is X% gender bias detected in the text. Changes are recommended."
+      />
+      <Profile
+      heading= "Race"
+      description= "There is X% racial bias detected in the text. Changes are recommended."
+      />
+      <Profile
+      heading= "Disability"
+      description= "There is X% disability bias detected in the text. Changes are recommended."
+      />
     </div>
   );
 }
---
+++ webapp/client/src/components/Profile.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./Profile.css"
+
+function Profile (props) {
+    return ( 
+        <div className="profile">
+            <p className="heading">{props.heading}</p>
+                <p className="description"> {props.description} </p>
+
+
+        </div>
+    )
+ }
+ export default Profile
