b0fe48d26f4d3a4ac36aed1421587358e5554009
Methasit Tantiplubtong <nokia.methasit@gmail.com>
2022-11-06 14:02:14 -0800

    percentages

--- webapp/client/src/App.js
+++ webapp/client/src/App.js
@@ -9,6 +9,10 @@ import { useState } from 'react';
 
 function App() {
   const [text, setText] = useState("");
+  const [fem, setFem] = useState(0);
+  const [mas, setMas] = useState(0);
+  const [race, setRace] = useState(0);
+  const [disability, setDisability] = useState(0);
 
   function textHandler(string){
     setText(string);
@@ -24,16 +28,24 @@ function App() {
         </div>
       <div className='profile_container'>
       <Profile
-      heading= "Gender"
-      description= "There is X% gender bias detected in the text. Changes are recommended."
+      heading= "Masculine"
+      description= "There is X% masculine bias detected in the text. Changes are recommended."
+      percentage = {mas}
+      />
+      <Profile
+      heading= "Feminine"
+      description= "There is X% feminine bias detected in the text. Changes are recommended."
+      percentage = {fem}
       />
       <Profile
       heading= "Race"
       description= "There is X% racial bias detected in the text. Changes are recommended."
+      percentage = {race}
       />
       <Profile
       heading= "Disability"
       description= "There is X% disability bias detected in the text. Changes are recommended."
+      percentage = {disability}
       />
       </div>
       </div>
