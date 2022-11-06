ec833ef88ba20c8041bc51bffdee1eb2a67633c9
Methasit Tantiplubtong <nokia.methasit@gmail.com>
2022-11-06 13:37:04 -0800

    text input state

--- webapp/client/src/components/Input.js
+++ webapp/client/src/components/Input.js
@@ -1,14 +1,24 @@
 import React from "react";
 import "./Input.css";
+import { useState } from "react";
 
 
 function Input(){
+    const [text, setText] = useState("");
+
+    function handleText(event){
+        event.preventDefault();
+        console.log(text);
+    }
+    
+    
+
     return (
         <div className="input">
             <p>Enter text here</p>
-            <form className="input_container">
-                <textarea className="input_box" type="text"></textarea>
-                <button className="button">Submit!</button>                 {/*add button function */}
+            <form className="input_container" onSubmit={handleText}>
+                <textarea onChange={e => setText(e.target.value)} className="input_box" type="text"></textarea>
+                <button className="button" type="submit">Submit!</button>                 {/*add button function */}
             </form>
 
         </div>
